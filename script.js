b72044b6ae8710e815113a4b2ba2238f4388c157
sreevethya19-ops <sreevethya19@gmail.com>
2025-09-09 15:16:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const quotes = [
+  "Believe you can and you're halfway there.",
+  "Your limitation—it’s only your imagination.",
+  "Push yourself, because no one else is going to do it for you.",
+  "Great things never come from comfort zones.",
+  "Dream it. Wish it. Do it.",
+  "Success doesn’t just find you. You have to go out and get it."
+];
+
+function generateQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quote").textContent = quotes[randomIndex];
+}
